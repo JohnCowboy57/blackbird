721e80eb7c67dc9621f1cb62800d0209368b23cb
OlegSpyq <oles022@mail.ru>
2025-11-16 16:24:26 +0300

    first

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+
+
+themeButton.onclick = function() {
+    page.classList.toggle('dark-theme');
+    page.classList.toggle('light-theme');
+};
+
+
